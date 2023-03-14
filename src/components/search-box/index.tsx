3a048125407bb1425292d9ff1bafae078c0311e8
Elon Aseneka <elonsantos63@gmail.com>
2023-03-14 13:51:17 +0300

    search for locations

--- src/components/search-box/index.tsx
+++ src/components/search-box/index.tsx
@@ -1,4 +1,4 @@
-import { component$, createContextId, Resource, useResource$, useStyles$, useSignal } from "@builder.io/qwik";
+import { component$, createContextId, Resource, useResource$, useStyles$, useSignal, useContextProvider, useStore } from "@builder.io/qwik";
 import styles from "./index.scss?inline";
 
 export interface SearchBoxProps { }
@@ -13,20 +13,20 @@ export type LocationData = {
     url: string;
 };
 
-export const LocationDataContext = createContextId<LocationData>('locationData');
+export const LocationDataContext = createContextId<LocationData[]>('locationData');
 
 export const getLocationData = async (search: string, controller?: AbortController) => {
     const url = `https://weatherapi-com.p.rapidapi.com/search.json?q=${search}`;
-    
+
     const response = await fetch(url, {
-        signal: controller?.signal, 
+        signal: controller?.signal,
         headers: {
             'X-RapidAPI-Key': 'b4f553c90bmsh592dd8c5885875bp1302a8jsn581b3431b255',
             'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
         },
     });
     const data = await response.json();
-    
+
     return data;
 };
 
@@ -35,6 +35,10 @@ export const getLocationData = async (search: string, controller?: AbortControll
 export const SearchBox = component$(() => {
     useStyles$(styles);
     const searchTerm = useSignal<string>('');
+
+    const locationData = useStore<Record<string, LocationData[]>>({
+        value: [],
+    })
 
 
     const weatherResource = useResource$<LocationData[]>(({ cleanup, track }) => {
@@ -45,44 +49,45 @@ export const SearchBox = component$(() => {
         const controller = new AbortController();
         cleanup(() => controller.abort());
         console.log('searchTerm.value', searchTerm.value);
-        
+
 
         // Fetch the data and return the promises.
         return getLocationData(searchTerm.value, controller);
     });
 
-    // const weatherStore = useStore<WeatherData>({
-    //     name: 'weatherStore',
-    // });
-
-    // useContextProvider(WeatherDataContext, weatherStore);
+    useContextProvider(LocationDataContext, locationData.value);
 
     return (
         <>
-            <h1>Search for your location</h1>
+            <div class="search-page">
+                <h1>Search for your location to get weather updates</h1>
 
-            <div>
-                <input type="text" value={searchTerm.value} onInput$={(e: Event) => (searchTerm.value = (e.target as HTMLInputElement).value)} />
+                <div class="input-container">
+                    <input type="text" value={searchTerm.value} onInput$={(e: Event) => (searchTerm.value = (e.target as HTMLInputElement).value)} />
+                </div>
+                <Resource
+                    value={weatherResource}
+                    onPending={() => <div>Loading...</div>}
+                    onRejected={() => <div>Failed to load weather</div>}
+                    onResolved={(locations) => {
+                        console.log('weather', locations);
+                        if (locations.length === 0) {
+                            return <h2>No data</h2>
+                        }
+
+                        locationData.value = locations;
+
+                        return <div class="results-container">
+
+                            <div class="dropdown">
+                                {locations.map && locations.map((item) => <p class="dropdown-select">{item.name}, {item.country}</p>)}
+                                {/* <p class="dropdown-select">item</p> */}
+                            </div>
+                        </div>;
+                    }}
+                />
             </div>
-            <Resource
-                value={weatherResource}
-                onPending={() => <div>Loading...</div>}
-                onRejected={() => <div>Failed to load weather</div>}
-                onResolved={(locations) => {
-                    console.log('weather', locations);
-                    if (locations.length === 0) {
-                        return <h2>No data</h2>
-                    }
-
-                    return <div class="search-container">
-
-                        <div class="dropdown">
-                            {locations.map && locations.map((item) => <p class="dropdown-select">{item.name}, {item.country}</p>)}
-                            {/* <p class="dropdown-select">item</p> */}
-                        </div>
-                    </div>;
-                }}
-            />
         </>
     );
 });
+
